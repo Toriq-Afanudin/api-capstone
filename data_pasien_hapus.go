6c6dccfa9b3cd9e7975b9668a8a0a33c10ad61ee
Toriq Afanudin <toriq1900006105@webmail.uad.ac.id>
2022-06-18 00:48:29 +0700

    Menambahkan CRUD untuk Data Pasien

---
+++ data_pasien_hapus.go
@@ -0,0 +1,20 @@
+(DELETE) https://api-capstone-heroku.herokuapp.com/auth/hapus_data_pasien/id
+
+contoh: https://api-capstone-heroku.herokuapp.com/auth/hapus_data_pasien/6 (tambahkan "id" di akhir link)
+
+respon:
+{
+    "data": {
+        "id": 4,
+        "nik": "330707879",
+        "nama": "Dhiya Aisy",
+        "alamat": "Wonogiri",
+        "jenis_kelamin": "P",
+        "no_hp": "087658291222",
+        "tempat_lahir": "Indramayu",
+        "tanggal_lahir": "1999-06-16"
+    },
+    "status": "berhasil menghapus data pasien",
+    "userID": "candra@gmail.com"
+}
+
