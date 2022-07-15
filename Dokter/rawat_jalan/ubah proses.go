c5d38e857d4e5df894c5996cf488d6abda85d76f
Toriq Afanudin <toriq1900006105@webmail.uad.ac.id>
2022-07-15 22:01:34 +0700

    update

--- Dokter/rawat_jalan/ubah proses.go
+++ Dokter/rawat_jalan/ubah proses.go
@@ -1,1 +1,15 @@
-(PUT) https://api-capstone-heroku.herokuapp.com/dokter/rawat_jalan_ubah_proses/36
+(PUT) https://api-capstone-heroku.herokuapp.com/dokter/rawat_jalan_ubah_proses/46
+
+body:
+{
+    "keterangan": "Minum obat 3x sehari",
+    "proses":false
+}
+
+respon:
+{
+    "code": 200,
+    "keterangan": "Minum obat 3x sehari",
+    "message": "Berhasil merubah keterangan dan proses.",
+    "proses": false
+}
