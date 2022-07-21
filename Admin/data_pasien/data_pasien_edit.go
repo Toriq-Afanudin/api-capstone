c3999070af2aa6c0394c29157f8b18b07137f3e5
Toriq Afanudin <toriq1900006105@webmail.uad.ac.id>
2022-07-21 16:22:23 +0700

    Menambahkan poli di edit data pasiens

--- Admin/data_pasien/data_pasien_edit.go
+++ Admin/data_pasien/data_pasien_edit.go
@@ -1,12 +1,14 @@
 (PUT) https://api-capstone-heroku.herokuapp.com/admin/data_pasien_edit/:id
 
-request contoh: https://api-capstone-heroku.herokuapp.com/admin/data_pasien_edit/5
+request contoh: https://api-capstone-heroku.herokuapp.com/admin/data_pasien_edit/70
 {
     "nik": "330709815",
     "nama": "Yusuf",
     "alamat": "Tuban, Banyuwangi",
     "jenis_kelamin": "P",
-    "nomer_telfon": "083720012964",
+    "poli":"Gigi",
+    "alamat":"",
+    "no_hp": "083720012964",
     "tempat_lahir": "Peterongan",
     "tanggal_lahir": "1999-12-18"
 }
